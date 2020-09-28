247899555d51866a463d2970f0bb27e71b815b9a
Rheeda-boo <rheeda.beecha@gmail.com>
2020-09-28 16:59:36 +0100

    Button classname given

--- src/Component/LogInForm.js
+++ src/Component/LogInForm.js
@@ -11,7 +11,7 @@ export default function LogInForm() {
                 <input type = "text" value = "" className = "userName" placeholder = "Username *" required = "true"/> <br/>
                 <label>Password: </label>
                 <input type = "password" value = "" className = "password" placeholder = "Password *" required = "true"/> <br/>
-                <Link to = "/home"><button value = "submit">Log In</button></Link>
+                <Link to = "/home"><button value = "submit" className = "button" >Log In</button></Link>
             </form>
             
         </div>
